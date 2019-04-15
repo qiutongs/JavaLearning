43837ea84b62d9639fbb97c87d27ba1c78b0e362
qiutongs <songqt01@gmail,com>
2019-04-14 21:44:48 -0700

    add sort

--- src/com/qiutongs/datastructure/list/SingleLinkedList.java
+++ src/com/qiutongs/datastructure/list/SingleLinkedList.java
@@ -2,100 +2,90 @@ package com.qiutongs.datastructure.list;
 
 public class SingleLinkedList<I> implements List<I> {
 
-	static class Node<I> {
-		private Node<I> next;
-		private I item;
+    private ListNode<I> head;
+    private ListNode<I> tail;
 
-		public Node(I item) {
-			this.next = null;
-			this.item = item;
-		}
+    public SingleLinkedList() {
+	head = tail = null;
+    }
 
-		@Override
-		public String toString() {
-			return item.toString();
-		}
+    @Override
+    public void append(I item) {
+	ListNode<I> n = new ListNode<>();
+	n.item = item;
+
+	if (tail == null) {
+	    head = tail = n;
+	    n.next = null;
+	} else {
+	    tail.next = n;
+	    n.next = null;
+	    tail = n;
+	}
+    }
+
+    @Override
+    public void insertHead(I item) {
+	// TODO Auto-generated method stub
+    }
+
+    @Override
+    public I search(I item) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public I removeTail() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder sb = new StringBuilder();
+
+	ListNode<I> curNode = head;
+
+	while (curNode != null) {
+	    sb.append(curNode + " -> ");
+	    curNode = curNode.next;
 	}
 
-	private Node<I> head;
-	private Node<I> tail;
+	sb.append("null");
 
-	public SingleLinkedList() {
-		head = tail = null;
+	return sb.toString();
+    }
+
+    public ListNode<I> getHead() {
+	return head;
+    }
+    
+    public void reverse() {
+	ListNode<I> curNode = head, preNode = null;
+
+	while (curNode != null) {
+	    ListNode<I> tmpNext = curNode.next;
+	    curNode.next = preNode;
+
+	    preNode = curNode;
+	    curNode = tmpNext;
 	}
 
-	@Override
-	public void append(I item) {
-		Node<I> n = new Node<>(item);
+	head = preNode;
+    }
 
-		if (tail == null) {
-			head = tail = n;
-			n.next = null;
-		} else {
-			tail.next = n;
-			n.next = null;
-			tail = n;
-		}
-	}
+    public static void main(String[] args) {
+	SingleLinkedList<Integer> list = new SingleLinkedList<>();
 
-	@Override
-	public void insertHead(I item) {
-		// TODO Auto-generated method stub
-	}
+	list.append(1);
+	list.append(2);
+	list.append(3);
 
-	@Override
-	public I search(I item) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	System.out.println(list);
 
-	@Override
-	public I removeTail() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-
-		Node<I> curNode = head;
-
-		while (curNode != null) {
-			sb.append(curNode + " -> ");
-			curNode = curNode.next;
-		}
-
-		sb.append("null");
-
-		return sb.toString();
-	}
-
-	public void reverse() {
-		Node<I> curNode = head, preNode = null;
-
-		while (curNode != null) {
-			Node<I> tmpNext = curNode.next;
-			curNode.next = preNode;
-			
-			preNode = curNode;
-			curNode = tmpNext;
-		}
-		
-		head = preNode;
-	}
-
-	public static void main(String[] args) {
-		SingleLinkedList<Integer> list = new SingleLinkedList<>();
-		
-		list.append(1);
-		list.append(2);
-		list.append(3);
-		
-		System.out.println(list);
-		
-		list.reverse();
-		System.out.println(list);
-	}
+	list.reverse();
+	System.out.println(list);
+    }
 
 }
